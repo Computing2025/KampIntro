237faa8cca95ee64d7e4d176eca9966e28d1da74
VICTUS <VICTUS@LAPTOP-33B4AD38>
2024-01-30 01:25:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarDun = 7.45;
+            double dolarBugun = 7.72;
+
+            if(dolarBugun > dolarDun)
+            {
+                Console.WriteLine("Yukarı ok göster");
+            }
+            else if (dolarBugun < dolarDun)
+            {
+                Console.WriteLine("Aşağı ok göster");
+            } 
+            else 
+            {
+                Console.WriteLine("Yatay çizgi göster");
+            }
+        }
+    }
+}
+
+
